b14b6acdd811ea8220167113f11af2a31773a64b
vagrant <vagrant@precise64.(none)>
2015-01-28 15:30:38 +0000

    add mongoDb

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,29 +1,90 @@
 var express = require('express');
 var router = express.Router();
 var _ = require('lodash');
+var Review = require('../database/reviews');
 
-var reviews =  [{
-name: 'McDo',
-placeType: 'Fastfood',
-stars: 3
-},
-{
-name: 'McDo2',
-placeType: 'Fastfood',
-stars: 15
-},
-{
-name: 'McDo3',
-placeType: 'Fastfood',
-stars: 4
-}];
+
+
+router.route('/api/')
+
+.get(function (req, res) {
+	Review.find({}, function (err, reviews) {
+		if (err) {
+			res.status(500).send({'error': err});
+			} else {
+					res.send(reviews);
+				   }
+		});
+	});
+router.route('/api/')
+.post(function (req, res) {
+	if (req.body.name === undefined || req.body.placeType === undefined || req.body.stars === undefined){
+		res.status(400).json({error: 'Erreur niveau des paramètres'});
+	}else {
+		var newReview = {
+			name: req.body.name,
+			placeType: req.body.placeType,
+			stars: req.body.stars
+		};
+		Review.create(newReview, function (err, reviews) {
+		if (err) {
+			res.status(500).send({'error': err});
+			} else {
+					res.send(reviews);
+				   }
+		});
+	}
+	});
+
+router.route('/api/:id')
+.put(function (req, res) {
+	if (req.body.name === undefined || req.body.placeType === undefined || req.body.stars === undefined){
+		res.status(400).json({error: 'Erreur niveau des paramètres'});
+	}else {
+			var reviewid = {_id: req.params.id};
+			var newReview = {
+				name: req.body.name,
+				placeType: req.body.placeType,
+				stars: req.body.stars
+			}
+			console.log(newReview);
+	  		Review.findOneAndUpdate(reviewid, newReview, function (err, reviews) {
+	  		if (err) {
+				res.status(500).send({'error': err});
+			} else {
+					res.send('Update réussi');
+				   }
+	  		 });
+	}
+	});
+
+router.route('/api/:id')
+.get(function (req, res) {
+			var reviewid = req.params.id;
+	  		Review.findById(reviewid, function (err, reviews) {
+	  		if (err) {
+				res.status(500).send({'error': err});
+			} else {
+					res.send(reviews);
+				   }
+	  		 });
+	});
+
+router.route('/api/:id')
+.delete(function (req, res) {
+			var reviewid = req.params.id;
+	  		Review.findByIdAndRemove(reviewid, function (err, reviews) {
+	  		if (err) {
+				res.status(500).send({'error': err});
+			} else {
+					res.send('delete sucess');
+				   }
+	  		 });
+	});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('reviews', { title: 'Reviews',reviews: reviews });
-});
-
-router.get('/api/', function (req, res) {
-  res.send(reviews);
 });
 
 router.post('/api/', function (req, res) {
@@ -66,25 +127,7 @@ router.post('/api/:id', function (req, res) {
 			res.send(reviews);
 		}
 });
-router.put('/api/:id', function (req, res) {
-		if (req.body.name === undefined || req.body.placeType === undefined || req.body.stars === undefined){
-		res.status(400).json({error: 'Erreur niveau des paramètres'});
-	}else {
-		if (reviewid > 0 && reviewid<= reviews.length){
-			var newReview = {
-				name: req.body.name,
-				placeType: req.body.placeType,
-				stars: req.body.stars
-			}
-			var reviewid = req.params.id;
-			reviews[reviewid]= newReview;
-	  		res.send(reviews);
-		}else{
-			res.status(404).json({error: 'Erreur cet id ne peut être supprimé hors du tableau'});
-		}
-		
-	}
-});
+
 router.delete('/api/:id', function (req, res) {
 	var reviewid = req.params.id;
 	if (reviewid > 0 && reviewid<= reviews.length)
