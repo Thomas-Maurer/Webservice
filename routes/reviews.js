b15d97f57c00c7a390a31e793b52e7a42607671b
vagrant <vagrant@precise64.(none)>
2015-01-28 13:53:47 +0000

    Fix routes + add verif

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var _ = require('lodash');
+
 var reviews =  [{
 name: 'McDo',
 placeType: 'Fastfood',
@@ -25,7 +27,17 @@ router.get('/api/', function (req, res) {
 });
 
 router.post('/api/', function (req, res) {
-  res.send(req.body);
+	if (req.body.name === undefined || req.body.placeType === undefined || req.body.stars === undefined){
+		res.status(400).json({error: 'Erreur niveau des paramètres'});
+	}else {
+	var newReview = {
+		name: req.body.name,
+		placeType: req.body.placeType,
+		stars: req.body.stars
+	}
+	reviews.push(newReview);
+	res.send('Ajout réussi', reviews);
+  }
 });
 router.delete('/api/', function (req, res) {
   res.send(req.body);
@@ -33,27 +45,56 @@ router.delete('/api/', function (req, res) {
 
 router.get('/api/:id', function (req, res) {
 	var reviewid = req.params.id;
-  res.send(reviews[reviewid]);
+	if (reviewid > 0 && reviewid<= reviews.length){
+ 		 res.send(reviews[reviewid]);
+	}else{
+		res.status(404).json({error: 'Erreur cet id ne peut être supprimé hors du tableau'});
+	}
 });
 
 router.post('/api/:id', function (req, res) {
-	var reviewid = req.params.id;
-  res.send(req.body);
+	if (req.body.name === undefined || req.body.placeType === undefined || req.body.stars === undefined){
+		res.status(400).json({error: 'Erreur niveau des paramètres'});
+	}else {
+			var reviewid = req.params.id;
+			var newReview = {
+					name: req.body.name,
+					placeType: req.body.placeType,
+					stars: req.body.stars
+				}
+			reviews.push(newReview);
+			res.send(reviews);
+		}
 });
 router.put('/api/:id', function (req, res) {
-	var newReview = {
-		name: req.body.name,
-		placeType: req.body.placeType,
-		stars: req.body.stars
+		if (req.body.name === undefined || req.body.placeType === undefined || req.body.stars === undefined){
+		res.status(400).json({error: 'Erreur niveau des paramètres'});
+	}else {
+		if (reviewid > 0 && reviewid<= reviews.length){
+			var newReview = {
+				name: req.body.name,
+				placeType: req.body.placeType,
+				stars: req.body.stars
+			}
+			var reviewid = req.params.id;
+			reviews[reviewid]= newReview;
+	  		res.send(reviews);
+		}else{
+			res.status(404).json({error: 'Erreur cet id ne peut être supprimé hors du tableau'});
+		}
+		
 	}
-	var reviewid = req.params.id;
-	reviews[reviewid]= newReview;
-  res.send(reviews);
 });
 router.delete('/api/:id', function (req, res) {
 	var reviewid = req.params.id;
-	reviews.splice(reviewid,1)
-  res.send(reviews);
+	if (reviewid > 0 && reviewid<= reviews.length)
+	{
+		res.send(_.pullAt(reviews, reviewid));
+	}else
+	{
+		res.status(400).json({error: 'Erreur cet id ne peut être supprimé hors du tableau'});
+	}
+  
 });
 
 module.exports = router;
